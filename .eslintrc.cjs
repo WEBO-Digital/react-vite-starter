3154b1d090af3fa708df052bafa9264720592651
Sushant Rajbanshi <sushant.rajbanshi@webo.digital>
2024-04-18 13:15:28 +0545

    chore: Update ESLint configuration to include React plugins, TypeScript checks, and Prettier integration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,21 @@ module.exports = {
     "plugin:@typescript-eslint/strict-type-checked",
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "eslint-config-prettier",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
@@ -19,5 +33,15 @@ module.exports = {
     ],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
   },
 };
